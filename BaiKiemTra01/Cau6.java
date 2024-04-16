d1142b31b10c4e89776b328e52aa7ba6a371cfb6
HoangHiep <162075103+hiep232709@users.noreply.github.com>
2024-04-16 16:16:17 +0700

    Add files via upload

---
+++ BaiKiemTra01/Cau6.java
@@ -0,0 +1,10 @@
+package BaiKiemTra01;
+
+public class Cau6 {
+//    Câu 6 (Nâng cao)
+//    Viết chương trình nhập vào từ bàn phím email và số điện thoại, kiểm tra email và số điện thoại đó có hợp lệ không.
+//    - Email phải đúng định dạng
+//    - Số điện thoại có chứa từ 9 đến 11 chữ số
+//    Gợi ý: Sử dụng regex https://viettuts.vn/java/su-dung-regex-trong-java
+
+}
